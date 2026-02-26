06d8f28c04d501a15ab866bed50cb8ac5666c805
one <garcia47stand@gmail.com>
2026-02-26 10:14:01 +0900

    Add

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite' // 이 줄이 꼭 있어야 합니다!
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: './', // 핵심: 어떤 환경변수도 필요 없이 './'로 통일하세요.
+  // 핵심: 상대 경로 빌드. 이 앱이 어떤 폴더(/flower 등)에 있든 작동하게 합니다.
+  base: './', 
 })
