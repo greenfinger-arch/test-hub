a4bcd4b6ba800ff766a54230d76d66dc4152bb31
one <garcia47stand@gmail.com>
2026-03-04 17:31:36 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,7 @@ import { officesurvivalData } from "./data/tests/officesurvivalData";
 import { workerDateData } from "./data/tests/workerDateData";
 import { plantMatchData } from "./data/tests/plantMatchData";
 import { deskHealthData } from "./data/tests/deskHealthData";
+import { officePlantData } from "./data/tests/officePlantData";
 
 // 1. 전역 스타일 및 애니메이션
 const GlobalStyle = createGlobalStyle`
@@ -137,6 +138,7 @@ const testRegistry = {
   workerDate: workerDateData,
   plantMatch: plantMatchData,
   deskHealth: deskHealthData,
+  officePlant: officePlantData,
 };
 
 // [삽입 코드 1] 공개 가능한 테스트 키값만 추출 (isReady가 true인 것만)
