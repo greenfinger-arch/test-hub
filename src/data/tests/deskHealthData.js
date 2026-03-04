3b9240e20043170a401746af9ef2793c0dfbd1db
one <garcia47stand@gmail.com>
2026-03-04 14:34:54 +0900

    수정

--- src/data/tests/deskHealthData.js
+++ src/data/tests/deskHealthData.js
@@ -52,21 +52,21 @@ export const deskHealthData = {
       img: "/images/desk-res01.jpg",
       desc: "모니터와 물아일체 직전! 거북목과 라운드 숄더가 당신의 생산성을 갉아먹고 있군요. 지금 당장 모니터 받침대를 높이고 목을 뒤로 당기세요! 장비빨이 시급한 상황입니다.",
       ctaText: "거북목 방지 모니터암 확인하기",
-      affiliateLink: "https://link.coupang.com/a/your_id_1"
+      affiliateLink: "https://link.coupang.com/a/dXteYj"
     },
     ZEN: {
       name: "갓벽한 '데스크 테라피스트' 타입",
       img: "/images/desk-res02.jpg",
       desc: "바른 자세와 정리된 환경, 당신은 효율의 마스터군요! 지금의 환경을 유지하면서 조명이나 향기로 감성을 한 스푼 더해보세요. 업무 집중력이 우주 끝까지 상승할 거예요.",
       ctaText: "집중력을 높이는 데스크 조명 보기",
-      affiliateLink: "https://link.coupang.com/a/your_id_2"
+      affiliateLink: "https://link.coupang.com/a/dXtjuY"
     },
     CHAOS: {
       name: "창의적인 '혼돈의 마법사' 타입",
       img: "/images/desk-res03.jpg",
       desc: "정리되지 않은 책상 위에서 창의력이 샘솟지만, 몸은 비명을 지르고 있어요! 손목 터널 증후군이 오기 전에 마우스부터 버티컬로 바꿔보는 건 어떨까요? 작은 변화가 큰 건강을 만듭니다.",
       ctaText: "손목 편한 버티컬 마우스 구경하기",
-      affiliateLink: "https://link.coupang.com/a/your_id_3"
+      affiliateLink: "https://link.coupang.com/a/dXtmy2"
     }
   }
 };
