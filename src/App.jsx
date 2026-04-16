2cdacf4964bc47ee519d7bff53b6af0af9d7b9c4
one <garcia47stand@gmail.com>
2026-04-16 10:56:58 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,7 @@ import { koreaApartmentFortuneData } from "./data/tests/koreaApartmentFortuneDat
 import { resumePassFortuneData } from "./data/tests/resumePassFortuneData.js";
 import { storeSuccessFortuneData } from "./data/tests/storeSuccessFortuneData.js";
 import { smartphoneWealthFortuneData } from "./data/tests/smartphoneWealthFortuneData.js";
+import { relationshipWealthFortuneData } from "./data/tests/relationshipWealthFortuneData.js";
 
 // 1. 전역 스타일 및 애니메이션
 const GlobalStyle = createGlobalStyle`
@@ -179,6 +180,7 @@ const testRegistry = {
   resumePassFortune: resumePassFortuneData,
   storeSuccessFortune: storeSuccessFortuneData,
   smartphoneWealthFortune: smartphoneWealthFortuneData,
+  relationshipWealthFortune: relationshipWealthFortuneData,
 };
 
 // [삽입 코드 1] 공개 가능한 테스트 키값만 추출 (isReady가 true인 것만)
